3b8c3cd28a7ff72a64d1955636c16a7e06bd71d3
LeeShinHaeng <dragon11796@gmail.com>
2024-08-26 10:44:27 +0900

    java ex inheritance example 2

---
+++ javaEx/src/main/java/org/example/ex03/Calculator.java
@@ -0,0 +1,8 @@
+package org.example.ex03;
+
+public class Calculator {
+    public double areaCircle(double radius) {
+        System.out.println("Calculator.areaCircle");
+        return 3.14 * radius * radius;
+    }
+}
