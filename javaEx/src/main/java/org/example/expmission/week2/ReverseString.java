c2f839cf828de6c1931c89b45a32a69ed1d8280c
LeeShinHaeng <dragon11796@gmail.com>
2024-08-28 11:26:07 +0900

    [exp mission] 240828 fix

--- javaEx/src/main/java/org/example/expmission/week2/ReverseString.java
+++ javaEx/src/main/java/org/example/expmission/week2/ReverseString.java
@@ -6,8 +6,16 @@ public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if(chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] += 32;
+            } else if(chars[i] >= 'a' && chars[i] <= 'z') {
+                chars[i] -= 32;
+            }
+            sb.append(chars[i]);
+        }
         System.out.println(sb);
         sc.close();
     }
