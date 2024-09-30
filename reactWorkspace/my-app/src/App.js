de12f344156c955a1c0ba13a3b30514a7ab2232f
LeeShinHaeng <dragon11796@gmail.com>
2024-09-30 11:00:55 +0900

    react: react 초기화
    240930

---
+++ reactWorkspace/my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Subject from './components/Subject';
+import Content from './components/Content';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>안녕하세요</h1>
+      {/* 컴포넌트 사용 */}
+      <Subject></Subject>
+      <hr></hr>
+      <Nav></Nav>
+      <hr></hr>
+      <Content></Content>
+    </div>
+  );
+}
+
+export default App;
