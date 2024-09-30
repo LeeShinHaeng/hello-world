d588f94f1931d57d6169f08882f1aa216c540392
LeeShinHaeng <dragon11796@gmail.com>
2024-09-30 09:56:42 +0900

    mysql: post CRUD 추가
    - 삭제 메서드 생성
    - Status 별로 조회
    
    240930

--- myboard/src/main/java/com/example/myboard/post/db/PostRepository.java
+++ myboard/src/main/java/com/example/myboard/post/db/PostRepository.java
@@ -3,6 +3,15 @@ package com.example.myboard.post.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    // select * from post where id=? and status=? order by id desc limit 1
+    Optional<PostEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, String status);
+
+    // select * from post where status=? order by id
+    List<PostEntity> findAllByStatusOrderByIdDesc(String status);
 }
