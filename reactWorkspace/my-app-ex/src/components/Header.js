a6c2d51ea8c20842afe4cdc82f1dc5b98ebf2e1b
LeeShinHaeng <dragon11796@gmail.com>
2024-09-30 13:22:29 +0900

    exp mission: 리액트 프로젝트 생성
    240930

---
+++ reactWorkspace/my-app-ex/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <h1>여기는 헤더 입니다.</h1>
+            <p>예시 텍스트 자리입니다.</p>
+        </div>
+    );
+}
+
+export default Header
