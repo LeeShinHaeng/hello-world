5f1e9d1a78e68158912bbb821b29cec894cbff8f
LeeShinHaeng <dragon11796@gmail.com>
2024-10-15 09:27:37 +0900

    react: 토글 버튼 예제
    241015

--- reactWorkspace/my-react-app/src/App.js
+++ reactWorkspace/my-react-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import RandomNumber from './components/RandomNumber';
+import Toggle from './components/Toggle';
 import './App.css'; 
 
 function App() {
   return (
     <div>
-      <RandomNumber />
+      <Toggle />
+
+      {/* <RandomNumber /> */}
     </div>
   );
 }
