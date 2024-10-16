21fe190077ab1a9371ce88b294538794b0e79fcc
LeeShinHaeng <dragon11796@gmail.com>
2024-10-16 10:25:01 +0900

    react: 훅 useCallback

--- reactWorkspace/hooks-tutorial/src/App.js
+++ reactWorkspace/hooks-tutorial/src/App.js
@@ -2,16 +2,19 @@ import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import Info from './components/Info';
+import Average from './components/Average';
 
 function App() {
   const [visible, setVisible] = useState(false);
 
   return (
     <div className="App">
-        <Counter />
+      <Average />
+      <hr></hr>
+        {/* <Counter />
         <hr></hr>
         <button onClick={() => {setVisible(!visible)}} >{visible ? "숨기기" : "보이기"}</button>
-        {visible && <Info />}
+        {visible && <Info />} */}
     </div>
   );
 }
