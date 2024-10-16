78d680a82ef6d7ee95f7ee479b14d96d5a99ca61
LeeShinHaeng <dragon11796@gmail.com>
2024-10-16 09:59:08 +0900

    react: hook 예시

--- reactWorkspace/hooks-tutorial/src/App.js
+++ reactWorkspace/hooks-tutorial/src/App.js
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import Info from './components/Info';
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="App">
         <Counter />
         <hr></hr>
-        <Info />
+        <button onClick={() => {setVisible(!visible)}} >{visible ? "숨기기" : "보이기"}</button>
+        {visible && <Info />}
     </div>
   );
 }
--- reactWorkspace/hooks-tutorial/src/components/Info.js
+++ reactWorkspace/hooks-tutorial/src/components/Info.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
-    const [name, setName] = useState("");
-    const [nickname, setNickname] = useState("");
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    console.log("랜더링이 완료되었습니다!");
+    console.log(name, nickname);
+    return () => {
+        console.log("unmount")
+    };
+  }, [name, nickname]);
 
-    const onChangeName = (e) => {
-        setName(e.target.value);
-    }
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
 
-    const onChangeNickname = (e) => {
-        setNickname(e.target.value);
-    }
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
 
-    return (
-        <div>
-        <div>
-            <input value={name} onChange={onChangeName} />
-            <input value={nickname} onChange={onChangeNickname} />      
-        </div>
-        <div>
-            <div>이름: {name}</div>
-            <div>닉네임: {nickname}</div>
-        </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>이름: {name}</div>
+        <div>닉네임: {nickname}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
