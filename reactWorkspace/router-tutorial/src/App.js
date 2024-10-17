55bd86e9b0d7b0fa84f3dfccf7cbd4950784a34b
LeeShinHaeng <dragon11796@gmail.com>
2024-10-17 10:34:49 +0900

    react: Hooks & 라우터 실습

--- reactWorkspace/router-tutorial/src/App.js
+++ reactWorkspace/router-tutorial/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Product from "./components/Product";
+import Product2 from "./components/Product2";
+import Product3 from "./components/Product3";
 import PageNotFound from "./components/PageNotFound"
 
 const App = () => {
@@ -17,6 +19,8 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/info" element={<About />} />
         <Route path="/product/:productId" element={<Product />} />
+        <Route path="/product2/:productId" element={<Product2 />} />
+        <Route path="/product3/:productId" element={<Product3 />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </div>
